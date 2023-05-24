ec769ae0e9200eeb4b769d902884f3eb5b640c69
Igor <105238566+IgorZhyvotkov@users.noreply.github.com>
2023-05-24 19:27:32 +0300

    newComponent

---
+++ src/components/Feedback/Feedback.styled.js
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+
+export const Section = styled.section`
+
+`;
+
+export const FeedbackHeader = styled.h2`
+
+`;
+
+export const FeedbackButtonList = styled.ul`
+padding-left: 15px;
+list-style: none;
+display: flex;
+gap: 10px;
+`;
+
+export const FeedbackButtonListItem = styled.ul`
+
+`;
+
+
+export const Button = styled.button`
+height: 28px;
+width: 70px;
+
+`;
+
+export const StatisticksList = styled.ul`
+list-style: none;
+`;
+
+export const StatisticksListItem = styled.li`
+
+`;
